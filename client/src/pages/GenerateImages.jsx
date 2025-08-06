ae472dca662cb11b1a1db93b0ff46adc2d6d34ed
Dipak <itzdipak0304@gmail.com>
2025-08-06 20:51:57 +0545

    Update GenerateImages.jsx

--- client/src/pages/GenerateImages.jsx
+++ client/src/pages/GenerateImages.jsx
@@ -15,6 +15,52 @@ const GenerateImages = () => {
   return (
      <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4
     text-slate-700'>
+      {/* limport { useAuth } from '@clerk/clerk-react';
+import axios from 'axios';
+import { Image, Sparkles } from 'lucide-react'
+import React, { useState } from 'react'
+import toast from 'react-hot-toast';
+
+
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+
+const GenerateImages = () => {
+   const ImageStyle  = ['Realistic','Ghibli style','Anime style','Cartoon style','Fantasy style','Realistic style','3D style','Portrait style']
+     
+    
+     const [selectedStyle, setSelectedStyle] = useState('Realistic')
+     const [input, setInput] = useState('')
+     const [publish, setPublish] =useState(false)
+      const [loading, setLoading] = useState(false);
+  const [content, setContent] = useState('');
+
+  const { getToken } = useAuth();
+    
+     const onSubmitHandler = async (e)=>{
+      e.preventDefault();
+      try {
+        setLoading(true)
+
+        const prompt=`Generate an image of ${input} in style ${selectedStyle}`
+
+        const {data} =await axios.post('/api/ai/generate-image', {prompt, publish},{headers: {Authorization:`Bearer ${await getToken()}`}})
+
+      if (data.success) {
+        setContent(data.content)
+      }else{
+        toast.error(data.message)
+      }
+        
+      } catch (error) {
+        toast.error(error.message)
+        
+      }
+      setLoading(false)
+     }
+  return (
+     <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4
+    text-slate-700'>
       {/* left col */}
       <form onSubmit={onSubmitHandler}  className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200' >
        <div className='flex items-center gap-3'>
@@ -51,10 +97,11 @@ const GenerateImages = () => {
        </div>
 
        
-       <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r
+       <button disabled={loading} className='w-full flex justify-center items-center gap-2 bg-gradient-to-r
        from-[#00AD25] to-[#04FF50] text-white px-4 py-2 mt-6 text-sm
        rounded-lg cursor-pointer' >
-        <Image className='w-5' />
+        {loading ? <span className='w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin'></span> :  <Image className='w-5' />}
+       
         Generate image
        </button> 
 
@@ -70,7 +117,10 @@ const GenerateImages = () => {
 
 
         </div>
-        <div className='flex-1 flex justify-center items-center'>
+        {
+          !content ? (
+
+               <div className='flex-1 flex justify-center items-center'>
           <div className='text-sm flex justify-center items-center gap-5 text-gray-400'>
 
              <Image className='w-9 h-9 ' />
@@ -78,6 +128,13 @@ const GenerateImages = () => {
                   
           </div>
         </div>
+          ) : (
+            <div className='mt-3 h-full'>
+              <img src={content} alt="image" className='w-full h-full' />
+            </div>
+          )
+        }
+        
       </div>
       
     </div>
