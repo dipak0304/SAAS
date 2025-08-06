f47609549d84885518290bc0af227f68928e6a04
Dipak <itzdipak0304@gmail.com>
2025-08-06 20:48:21 +0545

    Update BlogTitles.jsx

--- client/src/pages/BlogTitles.jsx
+++ client/src/pages/BlogTitles.jsx
@@ -23,10 +23,21 @@ const BlogTitles = () => {
     e.preventDefault();
     try {
       setLoading(true)
+      const prompt=`Generate a blog title for the keyword ${input} in the category ${selectedCategory}`
+
+      const {data} =await axios.post('/api/ai/generate-blog-title', {prompt},{headers: {Authorization:`Bearer ${await getToken()}`}})
+
+      if (data.success) {
+        setContent(data.content)
+      }else{
+        toast.error(data.message)
+      }
       
     } catch (error) {
+      toast.error(error.message)
       
     }
+    setLoading(false)
    }
   return (
      <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4
@@ -53,10 +64,11 @@ const BlogTitles = () => {
         ) )}
        </div>
        <br />
-       <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r
+       <button  disabled={loading} className='w-full flex justify-center items-center gap-2 bg-gradient-to-r
        from-[#C341F6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm
        rounded-lg cursor-pointer' >
-        <Hash className='w-5' />
+       
+        {loading ? <span className='w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin'></span> :  <Hash className='w-5' />}
         Generate title
        </button> 
 
@@ -72,7 +84,9 @@ const BlogTitles = () => {
 
 
         </div>
-        <div className='flex-1 flex justify-center items-center'>
+        {
+          !content ? (
+            <div className='flex-1 flex justify-center items-center'>
           <div className='text-sm flex justify-center items-center gap-5 text-gray-400'>
 
              <Hash className='w-9 h-9 ' />
@@ -80,6 +94,17 @@ const BlogTitles = () => {
                   
           </div>
         </div>
+
+          ): (
+            
+                     <div className="mt-3 h-full overflow-y-scroll text-sm text-slate-600">
+                       <div className="reset-tw"> {/* Apply your custom class here */}
+                         <Markdown>{content}</Markdown>
+                       </div>
+                     </div>
+          )
+        }
+        
       </div>
       
     </div>
