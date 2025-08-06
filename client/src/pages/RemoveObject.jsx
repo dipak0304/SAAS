5520732a9e26bd4cc195deb1d840912430f44e76
Dipak <itzdipak0304@gmail.com>
2025-08-06 20:47:36 +0545

    Update RemoveObject.jsx

--- client/src/pages/RemoveObject.jsx
+++ client/src/pages/RemoveObject.jsx
@@ -1,12 +1,49 @@
+import { useAuth } from '@clerk/clerk-react';
+import axios from 'axios';
 import { Scissors, Sparkles } from 'lucide-react';
 import React, { useState } from 'react'
+import toast from 'react-hot-toast';
+
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const RemoveObject = () => {
    const [input, setInput] = useState('')
    const [object, setObject] =useState('')
+   const [loading, setLoading] = useState(false);
+  const [content, setContent] = useState('');
+
+  const { getToken } = useAuth();
     
      const onSubmitHandler = async (e)=>{
       e.preventDefault();
+
+      try {
+        
+        setLoading(true)
+
+        if(object.split(' ').length > 1){
+          return toast('Please enter only one object name')
+        }
+
+        const formData =new FormData()
+        formData.append('image', input)
+         formData.append('object', object)
+
+
+        const {data} =await axios.post('/api/ai/remove-image-object', formData, {headers: {Authorization:`Bearer ${await getToken()}`}})
+
+      if (data.success) {
+        setContent(data.content)
+      }else{
+        toast.error(data.message)
+      }
+      
+      } catch (error) {
+        toast.error(error.message)
+        
+      }
+      setLoading(false)
      }
   return (
    <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4
@@ -24,16 +61,19 @@ const RemoveObject = () => {
 
 
         <p className='mt-6 text-sm font-medium'>Describe object name to remove</p>
-       <textarea onChange={(e)=>setObject(e.target.value)} value={input} rows={4}  
+       <textarea onChange={(e)=>setObject(e.target.value)} value={object} rows={4}  
        className='w-full p-2 px-3 mt-2 outline-none text-sm 
        rounded-md border border-gray-300' placeholder='e.g., watch or spoon, Only single object name' required />
 
        
       
-       <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r
+       <button disabled={loading} className='w-full flex justify-center items-center gap-2 bg-gradient-to-r
        from-[#417DF6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm
        rounded-lg cursor-pointer' >
-        <Scissors className='w-5 ' />
+        {
+          loading ? <span className='w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin'></span> :<Scissors className='w-5 ' />
+        }
+        
         Remove Object
        </button> 
 
@@ -49,7 +89,9 @@ const RemoveObject = () => {
 
 
         </div>
-        <div className='flex-1 flex justify-center items-center'>
+        {
+          !content ? (
+             <div className='flex-1 flex justify-center items-center'>
           <div className='text-sm flex justify-center items-center gap-5 text-gray-400'>
 
              <Scissors className='w-9 h-9 ' />
@@ -57,6 +99,13 @@ const RemoveObject = () => {
                   
           </div>
         </div>
+
+          ) :
+          (
+            <img src={content} alt="image" className='mt-3 w-full h-full' />
+          )
+        }
+       
       </div>
       
     </div>
@@ -64,3 +113,4 @@ const RemoveObject = () => {
 }
 
 export default RemoveObject
+
